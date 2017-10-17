5bbf23e878483246830dd4c559ae3ff10237dc87
merfemor <efremov.roman98@gmail.com>
2017-10-17 15:10:07 +0300

    added group img

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
+import {Parallax} from "ngx-parallax";
 
 import {AppComponent} from './app.component';
 
 @NgModule({
-    declarations: [AppComponent],
+    declarations: [AppComponent, Parallax],
     imports: [BrowserModule],
     bootstrap: [AppComponent]
 })
