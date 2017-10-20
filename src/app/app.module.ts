aa18f5e2a346fe434789c313df75744a3a776f85
merfemor <efremov.roman98@gmail.com>
2017-10-20 22:21:40 +0300

    moved header to component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import {NgModule} from '@angular/core';
 import {Parallax} from "ngx-parallax";
 
 import {AppComponent} from './app.component';
+import {HeaderComponent} from './header/header.component';
 
 @NgModule({
-    declarations: [AppComponent, Parallax],
+    declarations: [AppComponent, Parallax, HeaderComponent],
     imports: [BrowserModule],
     bootstrap: [AppComponent]
 })
