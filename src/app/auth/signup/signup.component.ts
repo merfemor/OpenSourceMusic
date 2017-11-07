775d3fb5287daa091ad7277014abf1242de12990
merfemor <efremov.roman98@gmail.com>
2017-11-08 01:05:27 +0300

    added redirecting in sign up

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -2,6 +2,7 @@ import {Component} from '@angular/core';
 import {UserService} from "../../user.service";
 import {User} from "../../api";
 import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
+import {Router} from "@angular/router";
 
 @Component({
     selector: 'app-auth',
@@ -13,7 +14,7 @@ export class SignUpComponent {
     user = new User();
     form: FormGroup;
 
-    constructor(private fb: FormBuilder, private userService: UserService) {
+    constructor(private router: Router, private fb: FormBuilder, private userService: UserService) {
         this.form = new FormGroup({
             'username': new FormControl(this.user.username, [
                 Validators.required
@@ -29,7 +30,13 @@ export class SignUpComponent {
     }
 
     onSubmit() {
-        console.log(this.form);
+        this.user.email = this.form.get('email').value;
+        this.user.username = this.form.get('username').value;
+        this.user.password = this.form.get('password').value;
+        if (this.userService.signUp(this.user)) {
+            this.router.navigateByUrl("/");
+        } else {
+            alert('Unknown error during authorization. Try again.');
+        }
     }
-
 }
