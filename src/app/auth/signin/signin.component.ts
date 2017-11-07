2547dff86e984c3382c0c97b2e24d76247fd7d08
merfemor <efremov.roman98@gmail.com>
2017-11-08 02:18:27 +0300

    made sign in form validation

--- src/app/auth/signin/signin.component.ts
+++ src/app/auth/signin/signin.component.ts
@@ -1,16 +1,36 @@
-import {Component, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
+import {UserService} from "../../user.service";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {Router} from "@angular/router";
+import {AuthorizationStatus} from "../../api";
 
 @Component({
     selector: 'app-auth',
     templateUrl: './signin.component.html',
-    styleUrls: ['./signin.component.sass']
+    styleUrls: ['./signin.component.sass'],
+    providers: [UserService]
 })
-export class SignInComponent implements OnInit {
+export class SignInComponent {
+    form = new FormGroup({
+        'login': new FormControl(null, [
+            Validators.required
+        ]),
+        'password': new FormControl(null, [
+            Validators.required
+        ])
+    });
+    authStatus: AuthorizationStatus = null;
 
-    constructor() {
+    constructor(private router: Router, private userService: UserService) {
     }
 
-    ngOnInit() {
+    singIn() {
+        let login = this.form.get('login').value;
+        let password = this.form.get('password').value;
+        this.authStatus = this.userService.signIn(login, password);
+        if (this.authStatus.successful) {
+            this.router.navigateByUrl("/");
+        }
     }
 
 }
