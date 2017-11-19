2c58cd408ecc0790f16a1a23934bcc1054d044e9
merfemor <efremov.roman98@gmail.com>
2017-11-19 15:37:34 +0300

    create/get projects methods in ProjectService

--- src/app/project.service.ts
+++ src/app/project.service.ts
@@ -1,9 +1,26 @@
 import {Injectable} from '@angular/core';
+import {Observable} from "rxjs/Observable";
+import {API_URL_ROOT, Project, RequestStatus} from "./api";
+import {HttpClient, HttpHeaders, HttpParams} from "@angular/common/http";
 
 @Injectable()
 export class ProjectService {
-
-    constructor() {
+    constructor(private http: HttpClient) {
     }
 
+    public getProjects(userId: number, madeByUser = true): Observable<Project[]> {
+        return this.http.get<Project[]>(API_URL_ROOT + "projects", {
+            params: new HttpParams()
+                .set("madeByUser", String(madeByUser))
+                .set("madeById", userId.toString())
+        });
+    }
+
+    public createProject(project: Project): Observable<RequestStatus> {
+        return this.http.post<Project>(API_URL_ROOT + "projects", project, {
+            headers: new HttpHeaders().set("Content-Type", "application/json")
+        }).map(project => project ?
+            {successful: true} :
+            {successful: false, description: "Unknown error"});
+    }
 }
