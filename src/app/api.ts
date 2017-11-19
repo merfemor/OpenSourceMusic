9ed55895afd3bb5c2bd7a7f9d78cf273b09f281f
merfemor <efremov.roman98@gmail.com>
2017-11-19 15:07:53 +0300

    added Project class in API

--- src/app/api.ts
+++ src/app/api.ts
@@ -1,13 +1,23 @@
 export const API_URL_ROOT = "http://localhost:3000/";
 
 export class User {
-    public id: number;
-    public email: string;
-    public password: string;
-    public firstName: string;
-    public lastName: string;
-    public username: string;
-    public profileImageUrl: string;
+    id: number;
+    email: string;
+    password: string;
+    firstName: string;
+    lastName: string;
+    username: string;
+    profileImageUrl: string;
+}
+
+export class Project {
+    id: number;
+    madeByUser: boolean;
+    madeById: number;
+    title: string;
+    description: string;
+    creationDate: Date;
+    isEmpty: boolean;
 }
 
 export class RequestStatus {
